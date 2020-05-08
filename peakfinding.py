b05af4b7cb0b29b06a56a2d087a56ff3de925305
David Linn <dlinn@hmc.edu>
2020-05-08 16:47:36 -0700

    Second commit

--- peakfinding.py
+++ peakfinding.py
@@ -25,19 +25,16 @@ p = [math.sin(2*math.pi*.01*i) for i in range(10000)]
 print(sps.find_peaks(p)) # expected at 25, 125, 225, ...
 print(find_peaks(p))
 print(find_peaks_2(p))
-n = [10,100,1000]
 t1 = []
 t2 = []
 t3 = []
-t1.append(timeit.timeit('sps.find_peaks(p)',number=10,globals=globals()))
-t2.append(timeit.timeit('find_peaks(p)',number=10,globals=globals()))
-t3.append(timeit.timeit('find_peaks_2(p)',number=10,globals=globals()))
-t1.append(timeit.timeit('sps.find_peaks(p)',number=100,globals=globals()))
-t2.append(timeit.timeit('find_peaks(p)',number=100,globals=globals()))
-t3.append(timeit.timeit('find_peaks_2(p)',number=100,globals=globals()))
-t1.append(timeit.timeit('sps.find_peaks(p)',number=1000,globals=globals()))
-t2.append(timeit.timeit('find_peaks(p)',number=1000,globals=globals()))
-t3.append(timeit.timeit('find_peaks_2(p)',number=1000,globals=globals()))
+n = [1,10,100,300,700,1000]
+for i in n:
+    t1.append(timeit.timeit('sps.find_peaks(p)',number=i,globals=globals()))
+    t2.append(timeit.timeit('find_peaks(p)',number=i,globals=globals()))
+    t3.append(timeit.timeit('find_peaks_2(p)',number=i,globals=globals()))
 plt.plot(n,t1,n,t2,n,t3)
 plt.legend(['SciPy','Implementation 1','Implementation 2'])
+plt.xlabel('Num Iterations')
+plt.ylabel('Time (s)')
 plt.show()
